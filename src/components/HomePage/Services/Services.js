b9fb7fa43789e5f2cf9d0662500742c2d69fffca
Mutasim-Shatil <mutasimshatil@hotmail.com>
2021-04-13 00:50:58 +0800

    design services section

--- src/components/HomePage/Services/Services.js
+++ src/components/HomePage/Services/Services.js
@@ -1,21 +1,41 @@
 // import React, { useState } from 'react'
 import serviceCompany from '../../../data/serviceCompany'
+import Service from '../../../data/services'
 import './Services.css'
 
 const Services = () => {
     const company= serviceCompany
     console.log(company);
+    const service = Service
     return (
-      <div className="services container">
-        {company.map((company, index) => {
-          return (
-            <div key={index}>
-              <div className="company">
-                <img src={company.img} alt="" />
+      <div className="container">
+        <div className="service">
+          {company.map((company, index) => {
+            return (
+              <div key={index}>
+                <div className="company">
+                  <img src={company.img} alt="" />
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
+        <div className="bottom-side">
+          <h4 className="services__title">
+            Provide awesome <span>services</span>
+          </h4>
+          <div className="services">
+            {service.map((service) => {
+              return (
+                <div className="services__details">
+                  <img src={service.img} alt="" />
+                  <h6>{service.title}</h6>
+                  <p>{service.details}</p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
     );
 }
---
+++ src/data/services.js
@@ -0,0 +1,21 @@
+import web from '../images/icons/service1.png'
+import graphics from '../images/icons/service2.png'
+import development from '../images/icons/service3.png'
+const Service = [
+  {
+    img: web,
+    title: "Web $ Mobile design",
+    details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
+  },
+  {
+    img: graphics,
+    title: "Graphics design",
+    details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
+  },
+  {
+    img: development,
+    title: "Web development",
+    details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
+  },
+];
+export default Service
